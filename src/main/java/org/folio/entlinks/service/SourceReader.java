5fc198c243703eca14f9b919383499a7bbe8cdf4
Pavlo Smahin <Pavlo_Smahin@epam.com>
2023-05-11 16:14:07 +0300

    moved to stream processing

--- src/main/java/org/folio/entlinks/service/SourceReader.java
+++ src/main/java/org/folio/entlinks/service/SourceReader.java
@@ -9,13 +9,15 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.output.FileWriterWithEncoding;
 import org.folio.Authority;
 import org.folio.processing.mapping.defaultmapper.MarcToAuthorityMapper;
 import org.folio.processing.mapping.defaultmapper.processor.parameters.MappingParameters;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -50,36 +52,49 @@ public class SourceReader {
       sb.append(String.format("marc_id < ?", chunk.endBy()));
     }
 
-    log.info("start::retrieve authority records from {} to {}", chunk.startFrom(), chunk.endBy());
-    var resultStream = getResultStream(chunk, sb);
-    log.info("finish::retrieve authority records from {} to {}", chunk.startFrom(), chunk.endBy());
-    log.info("start::map authority records from {} to {}", chunk.startFrom(), chunk.endBy());
-    var list = resultStream.stream()
-      .map(sourceData -> {
-        var marcSource = new JsonObject(sourceData.marc().toString());
-        var authority = mapper.mapRecord(marcSource, mappingParameters, mappingRules);
-        authority.setId(sourceData.authorityId());
-        authority.setVersion(sourceData.version());
-        authority.setSource(Authority.Source.MARC);
-        return authority;
-      })
-      .map(authority -> {
-        try {
-          return objectMapper.writeValueAsString(authority);
-        } catch (JsonProcessingException e) {
-          throw new RuntimeException(e);
-        }
-      })
-      .collect(Collectors.toList());
-    log.info("finish::map authority records from {} to {}", chunk.startFrom(), chunk.endBy());
-    writeToFile(list, file);
-    list.clear();
+    getAndSave(mappingRules, mappingParameters, objectMapper, chunk, file, sb);
     log.info("finishChunk_{}", chunk.id());
     return CompletableFuture.completedFuture(true);
   }
 
-  private List<SourceData> getResultStream(ChunkPreparation.Chunk chunk, StringBuilder sb) {
-    return jdbcTemplate.query(
+  @NotNull
+  private void getAndSave(JsonObject mappingRules, MappingParameters mappingParameters,
+                          ObjectMapper objectMapper, ChunkPreparation.Chunk chunk, File file,
+                          StringBuilder sb) {
+    try (var writer = new FileWriterWithEncoding(file, StandardCharsets.UTF_8, true);
+         var resultStream = getResultStream(chunk, sb)) {
+
+      resultStream
+        .map(sourceData -> {
+          var marcSource = new JsonObject(sourceData.marc().toString());
+          var authority = mapper.mapRecord(marcSource, mappingParameters, mappingRules);
+          authority.setId(sourceData.authorityId());
+          authority.setVersion(sourceData.version());
+          authority.setSource(Authority.Source.MARC);
+          return authority;
+        })
+        .map(authority -> {
+          try {
+            return objectMapper.writeValueAsString(authority);
+          } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+          }
+        })
+        .forEach(line -> write(writer, line));
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    } finally {
+      log.info("File saved: {}", file.getName());
+    }
+  }
+
+  @SneakyThrows
+  private void write(FileWriterWithEncoding writer, String line) {
+    writer.write(line + System.lineSeparator());
+  }
+
+  private Stream<SourceData> getResultStream(ChunkPreparation.Chunk chunk, StringBuilder sb) {
+    return jdbcTemplate.queryForStream(
       con -> {
         var ps = con.prepareStatement(sb.toString());
         if (chunk.startFrom() != null) {
@@ -105,7 +120,7 @@ public class SourceReader {
     log.info("Write {} lines to file: {}", s.size(), file.getName());
     try (var writer = new FileWriterWithEncoding(file, StandardCharsets.UTF_8, true)) {
       for (String s1 : s) {
-        writer.write(s1 + System.lineSeparator());
+        write(writer, s1);
       }
     } catch (IOException e) {
       throw new IllegalStateException(e);
