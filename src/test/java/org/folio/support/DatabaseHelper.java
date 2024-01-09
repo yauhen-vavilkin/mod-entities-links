d3bfc7d5586d71b89227b845660e6868d3acd032
Viacheslav Kolesnyk <94473337+viacheslavkol@users.noreply.github.com>
2024-01-09 15:22:24 +0100

    fix(authority-source-files): Fix enum usage for 'source' field of authority source files (#221)
    - Move current release changesets into an appropriate folder
    - Fix preconditions for changesets
    - Add changeSet to change 'source' column type for source files table to enum with migrating existing records
    
    Closes: MODELINKS-184

--- src/test/java/org/folio/support/DatabaseHelper.java
+++ src/test/java/org/folio/support/DatabaseHelper.java
@@ -19,6 +19,7 @@ public class DatabaseHelper {
   public static final String INSTANCE_AUTHORITY_LINK_TABLE = "instance_authority_link";
   public static final String AUTHORITY_NOTE_TYPE_TABLE = "authority_note_type";
   public static final String AUTHORITY_SOURCE_FILE_TABLE = "authority_source_file";
+  public static final String AUTHORITY_SOURCE_FILE_SOURCE_TYPE = "AuthoritySourceType";
   public static final String AUTHORITY_SOURCE_FILE_CODE_TABLE = "authority_source_file_code";
   public static final String AUTHORITY_TABLE = "authority";
   public static final String AUTHORITY_ARCHIVE_TABLE = "authority_archive";
@@ -32,24 +33,24 @@ public class DatabaseHelper {
     this.jdbcTemplate = jdbcTemplate;
   }
 
-  public String getTable(String tenantId, String table) {
-    return metadata.getDBSchemaName(tenantId) + "." + table;
+  public String getDbPath(String tenantId, String basePath) {
+    return metadata.getDBSchemaName(tenantId) + "." + basePath;
   }
 
   public int countRows(String tableName, String tenant) {
-    return JdbcTestUtils.countRowsInTable(jdbcTemplate, getTable(tenant, tableName));
+    return JdbcTestUtils.countRowsInTable(jdbcTemplate, getDbPath(tenant, tableName));
   }
 
   public int countRowsWhere(String tableName, String tenant, String whereClause) {
-    return JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, getTable(tenant, tableName), whereClause);
+    return JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, getDbPath(tenant, tableName), whereClause);
   }
 
   public void deleteFromTable(String tableName, String tenant) {
-    JdbcTestUtils.deleteFromTables(jdbcTemplate, getTable(tenant, tableName));
+    JdbcTestUtils.deleteFromTables(jdbcTemplate, getDbPath(tenant, tableName));
   }
 
   public void saveAuthorityNoteType(String tenant, AuthorityNoteType entity) {
-    var sql = "INSERT INTO " + getTable(tenant, AUTHORITY_NOTE_TYPE_TABLE)
+    var sql = "INSERT INTO " + getDbPath(tenant, AUTHORITY_NOTE_TYPE_TABLE)
       + " (id, name, source, created_date, updated_date, created_by_user_id, "
       + "updated_by_user_id) VALUES (?,?,?,?,?,?,?)";
     jdbcTemplate.update(sql, entity.getId(), entity.getName(), entity.getSource(), entity.getCreatedDate(),
@@ -57,16 +58,18 @@ public class DatabaseHelper {
   }
 
   public void saveAuthoritySourceFile(String tenant, AuthoritySourceFile entity) {
-    var sql = "INSERT INTO " + getTable(tenant, AUTHORITY_SOURCE_FILE_TABLE)
+    var sourceType = getDbPath(tenant, AUTHORITY_SOURCE_FILE_SOURCE_TYPE);
+    var sqlValues = "(?,?,?::" + sourceType + ",?,?,?,?,?,?,?)";
+    var sql = "INSERT INTO " + getDbPath(tenant, AUTHORITY_SOURCE_FILE_TABLE)
       + " (id, name, source, type, base_url, hrid_start_number, created_date, updated_date,"
-      + "created_by_user_id, updated_by_user_id) VALUES (?,?,?,?,?,?,?,?,?,?)";
+      + "created_by_user_id, updated_by_user_id) VALUES " + sqlValues;
     jdbcTemplate.update(sql, entity.getId(), entity.getName(),
         entity.getSource().name(), entity.getType(), entity.getBaseUrl(), entity.getHridStartNumber(),
         entity.getCreatedDate(), entity.getUpdatedDate(), entity.getCreatedByUserId(), entity.getUpdatedByUserId());
   }
 
   public String queryAuthoritySourceFileSequenceName(String tenant, UUID id) {
-    var sql = "SELECT * from " + getTable(tenant, AUTHORITY_SOURCE_FILE_TABLE) + " where id = '" + id + "'";
+    var sql = "SELECT * from " + getDbPath(tenant, AUTHORITY_SOURCE_FILE_TABLE) + " where id = '" + id + "'";
     return jdbcTemplate.query(sql, rs -> {
       if (rs.next()) {
         return rs.getString("sequence_name");
@@ -88,25 +91,25 @@ public class DatabaseHelper {
   }
 
   public void updateAuthorityNaturalId(String tenant, UUID authorityId, String naturalId) {
-    var sql = "UPDATE " + getTable(tenant, AUTHORITY_TABLE)
+    var sql = "UPDATE " + getDbPath(tenant, AUTHORITY_TABLE)
         + " SET natural_id = ? where id = ?";
     jdbcTemplate.update(sql, naturalId, authorityId);
   }
 
   public void updateAuthorityArchiveUpdateDate(String tenant, UUID authorityArchiveId, Timestamp updatedDate) {
-    var sql = "UPDATE " + getTable(tenant, AUTHORITY_ARCHIVE_TABLE)
+    var sql = "UPDATE " + getDbPath(tenant, AUTHORITY_ARCHIVE_TABLE)
         + " SET updated_date = ? where id = ?";
     jdbcTemplate.update(sql, updatedDate, authorityArchiveId);
   }
 
   public void saveAuthoritySourceFileCode(String tenant, UUID sourceFileId, AuthoritySourceFileCode code) {
-    var sql = "INSERT INTO " + getTable(tenant, AUTHORITY_SOURCE_FILE_CODE_TABLE)
+    var sql = "INSERT INTO " + getDbPath(tenant, AUTHORITY_SOURCE_FILE_CODE_TABLE)
       + " (authority_source_file_id, code) VALUES (?,?)";
     jdbcTemplate.update(sql, sourceFileId, code.getCode());
   }
 
   public void saveAuthority(String tenant, Authority entity) {
-    var sql = "INSERT INTO " + getTable(tenant, AUTHORITY_TABLE)
+    var sql = "INSERT INTO " + getDbPath(tenant, AUTHORITY_TABLE)
         +  " (id, _version, natural_id, source, heading, heading_type, subject_heading_code, created_date, "
         + "created_by_user_id, updated_date, updated_by_user_id, deleted, source_file_id) "
         + "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
@@ -120,7 +123,7 @@ public class DatabaseHelper {
   }
 
   public AuthorityNoteType getAuthorityNoteTypeById(UUID id, String tenant) {
-    String sql = "SELECT * FROM " + getTable(tenant, AUTHORITY_NOTE_TYPE_TABLE) + " WHERE id = '" + id + "'";
+    String sql = "SELECT * FROM " + getDbPath(tenant, AUTHORITY_NOTE_TYPE_TABLE) + " WHERE id = '" + id + "'";
     return jdbcTemplate.query(sql, rs -> {
       if (rs.next()) {
         var authorityNoteType = new AuthorityNoteType();
@@ -135,7 +138,7 @@ public class DatabaseHelper {
   }
 
   public void saveAuthorityReindexJob(String tenant, ReindexJob job) {
-    var sql = "INSERT INTO " + getTable(tenant, AUTHORITY_REINDEX_JOB_TABLE)
+    var sql = "INSERT INTO " + getDbPath(tenant, AUTHORITY_REINDEX_JOB_TABLE)
         + " (id, resource_name, job_status, published, submitted_date) VALUES (?,?,?,?,?)";
     jdbcTemplate.update(sql, job.getId(), job.getResourceName().name(), job.getJobStatus().name(),
         job.getPublished(), DateUtils.toTimestamp(job.getSubmittedDate()));
